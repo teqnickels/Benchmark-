e21e4bb1c684216aa8f7dd1c64ae590bcd939df7
Debrena McEwen <debrenamcewen@gmail.com>
2017-01-31 09:40:37 -0800

    add set up for testing

---
+++ src/lib/scrabble.js
@@ -0,0 +1,35 @@
+// create an object with key value pairs
+// each letter will have a value which is equal to the count of the letters in the bag
+//using prompt, ask the users which letters have already been played
+//it's always subtraction
+
+var scrabbleBag = {
+  9:'A',
+  2:'B',
+  2:'C',
+  4:'D',
+  12:'E',
+  2:'F',
+  3:'G',
+  2:'H',
+  9:'I',
+  1:'J',
+  1:'K',
+  4:'L',
+  2:'M',
+  6:'N',
+  8:'O',
+  2:'P',
+  1:'Q',
+  6:'R',
+  4:'S',
+  6:'T',
+  4:'U',
+  2:'V',
+  2:'W',
+  1:'X',
+  2:'Y',
+  1:'Z',
+  2:'_'
+}
+var userTally = prompt('Enter the scrabble tiles that have been played in ALL CAPS')
