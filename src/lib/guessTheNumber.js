135d0726a700bd00fb7d7147b8a54ac90d99a16a
Debrena McEwen <debrenamcewen@gmail.com>
2017-02-01 14:40:06 -0800

    complete scrabble bag program

--- src/lib/guessTheNumber.js
+++ src/lib/guessTheNumber.js
@@ -1,14 +1,34 @@
 "use strict";
-const playerNumber = prompt("I'm thinking of a number between 1 and 100. Can you guess the number I'm thinking of?")
 
-const min = 1
-const max = 100
+let guessTheNumber = () => {
+  let playerNumber = prompt("I'm thinking of a number between 1 and 100. Can you guess the number I'm thinking of?")
 
-let randomNumber =  Math.random() * (max-min) + min;
-let ourNumber  = Math.round(randomNumber)
+  const min = 1
+  const max = 100
 
-if (playerNumber === ourNumber) {
-  alert('Congratulations! You guessed the number we were thinking of!')
-} else {
-  alert(`Sorry, my number was ${ourNumber}`)
+  let randomNumber =  Math.random() * (max-min) + min;
+  let ourNumber  = Math.round(randomNumber)
+
+  if (playerNumber == ourNumber) {
+    return alert('Congratulations! You guessed the number we were thinking of!')
+  }else{
+
+    while(playerNumber !== ourNumber){
+
+      if(playerNumber !== null){
+        return
+      }
+
+      if(playerNumber > ourNumber) {
+          let playerNumber = prompt('Sorry, your number is too high, try again' )
+        }
+
+      if(playerNumber < ourNumber){
+        let playerNumber = prompt('Sorry, your number is too low, try again.')
+        }
+      if(playerNumber == nan){
+        prompt('Please enter a NUMBER')
+      }
+      }
+    }
 }
---
+++ src/spec/guessTheNumberSpec.js
@@ -0,0 +1,10 @@
+import chai, {expect} from 'chai'
+import chaiChange from 'chai-change'
+import guessTheNumber from '../lib/guessTheNumber'
+
+describe('guessTheNumber', () => {
+  it('if the user guess matches the generated number' () => {
+      expect(guessTheNumber().toEqual())
+
+  })
+})
