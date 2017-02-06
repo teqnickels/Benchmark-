7d1c34fbb6f48c5b51a5e32ffdcd3e52d5f75929
Debrena McEwen <debrenamcewen@gmail.com>
2017-02-06 08:48:51 -0800

    final try

--- src/lib/where.js
+++ src/lib/where.js
@@ -1,14 +1,20 @@
-
-
 const findThePath = (obj) =>{
   const data = JSON.stringify(obj)
   const json = JSON.parse(data)
-  let jsonStr =  Object.getOwnPropertyNames(json)
+  let jsonStr = Object.getOwnPropertyNames(json)
   let index = Object.values(json)
   let value = Object.values(jsonStr)
 
 
   jsonStr.forEach(function(jsonStr, index, value){
-    console.log(jsonStr + ' -> ' + index + ' -> ' + obj[jsonStr])
+
+    if( value.hasOwnProperty(value) ){
+
+      let nestedObj = Object.getOwnPropertyNames(value)
+      console.log(jsonStr + ' -> ' + index + ' -> ' + obj[jsonStr] + 'nestedObj')
+
+    }else{
+      console.log(jsonStr + ' -> ' + index + ' -> ' + obj[jsonStr])
+    }
   })
 }
